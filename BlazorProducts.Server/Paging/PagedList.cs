5cf93500c493865f4f92de40c5b55e0e02d582a9
Rafał Świrk <rafalswirk@outlook.com>
2021-09-15 06:00:24 +0200

    - Implementing generic class for paged list

---
+++ BlazorProducts.Server/Paging/PagedList.cs
@@ -0,0 +1,37 @@
+﻿using Entities.RequestFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Server.Paging
+{
+    public class PagedList<T> : List<T>
+    {
+        public Metadata MetaData { get; set; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            MetaData = new Metadata
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+            };
+
+            AddRange(items);
+        }
+
+        public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
